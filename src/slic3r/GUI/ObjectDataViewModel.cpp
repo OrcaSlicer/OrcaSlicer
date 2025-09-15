879838c83a1eae7f461fc0a5403567643b40de99
Raoul Rubien <rubienr@sbox.tugraz.at>
2025-09-15 21:54:12 +0200

    fixes: pointer used after ‘void operator delete(void*, std::size_t)’ [-Wuse-after-free]

--- src/slic3r/GUI/ObjectDataViewModel.cpp
+++ src/slic3r/GUI/ObjectDataViewModel.cpp
@@ -1052,13 +1052,12 @@ wxDataViewItem ObjectDataViewModel::Delete(const wxDataViewItem &item)
         ObjectDataViewModelNode* last_instance_node = node_parent->GetNthChild(0);
         PrintIndicator last_instance_printable = last_instance_node->IsPrintable();
         node_parent->GetChildren().Remove(last_instance_node);
-        delete last_instance_node;
         ItemDeleted(parent, wxDataViewItem(last_instance_node));
+        delete last_instance_node;
 
         ObjectDataViewModelNode* obj_node = node_parent->GetParent();
         obj_node->set_printable_icon(last_instance_printable);
         obj_node->GetChildren().Remove(node_parent);
-        delete node_parent;
         ret_item = wxDataViewItem(obj_node);
 
 #ifndef __WXGTK__
@@ -1066,6 +1065,7 @@ wxDataViewItem ObjectDataViewModel::Delete(const wxDataViewItem &item)
             obj_node->m_container = false;
 #endif //__WXGTK__
         ItemDeleted(ret_item, wxDataViewItem(node_parent));
+        delete node_parent;
         return ret_item;
     }
 
@@ -1077,7 +1077,6 @@ wxDataViewItem ObjectDataViewModel::Delete(const wxDataViewItem &item)
     {
         ObjectDataViewModelNode* obj_node = node_parent->GetParent();
         obj_node->GetChildren().Remove(node_parent);
-        delete node_parent;
         ret_item = wxDataViewItem(obj_node);
 
 #ifndef __WXGTK__
@@ -1085,6 +1084,7 @@ wxDataViewItem ObjectDataViewModel::Delete(const wxDataViewItem &item)
             obj_node->m_container = false;
 #endif //__WXGTK__
         ItemDeleted(ret_item, wxDataViewItem(node_parent));
+        delete node_parent;
         return ret_item;
     }
 
