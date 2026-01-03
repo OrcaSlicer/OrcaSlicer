5f29f5e004fb097f96be620c635274a52db39cba
SoftFever <softfeverever@gmail.com>
2026-01-03 22:57:34 +0800

    1. Pass wxMouseEvent by ref in HyperLink 2. Changed SetFont method to return a boolean for better error handling.

--- src/slic3r/GUI/Widgets/HyperLink.cpp
+++ src/slic3r/GUI/Widgets/HyperLink.cpp
@@ -1,20 +1,14 @@
 #include "HyperLink.hpp"
+#include "Label.hpp"
 
-namespace Slic3r {
-namespace GUI {
+namespace Slic3r { namespace GUI {
 
-HyperLink::HyperLink(
-    wxWindow* parent,
-    const wxString& label,
-    const wxString& url,
-    long style
-)
+HyperLink::HyperLink(wxWindow* parent, const wxString& label, const wxString& url, long style)
     : wxStaticText(parent, wxID_ANY, label)
     , m_url(url)
     , m_normalColor(wxColour("#009687")) // used slightly different color otherwise automatically uses ColorForDark that not visible enough
-    , m_hoverColor( wxColour("#26A69A"))
+    , m_hoverColor(wxColour("#26A69A"))
 {
-
     SetForegroundColour(m_normalColor);
     HyperLink::SetFont(Label::Head_14);
     SetCursor(wxCursor(wxCURSOR_HAND));
@@ -22,30 +16,34 @@ HyperLink::HyperLink(
     if (!m_url.IsEmpty())
         SetToolTip(m_url);
 
-    Bind(wxEVT_LEFT_DOWN   ,([this](wxMouseEvent e) {
-        if (!m_url.IsEmpty())
-            wxLaunchDefaultBrowser(m_url);
-    }));
+    Bind(wxEVT_LEFT_DOWN, ([this](wxMouseEvent& e) {
+             if (!m_url.IsEmpty())
+                 wxLaunchDefaultBrowser(m_url);
+         }));
 
-    Bind(wxEVT_ENTER_WINDOW,([this](wxMouseEvent e) {SetForegroundColour(m_hoverColor ); Refresh();}));
-    Bind(wxEVT_LEAVE_WINDOW,([this](wxMouseEvent e) {SetForegroundColour(m_normalColor); Refresh();}));
+    Bind(wxEVT_ENTER_WINDOW, ([this](wxMouseEvent& e) {
+             SetForegroundColour(m_hoverColor);
+             Refresh();
+         }));
+    Bind(wxEVT_LEAVE_WINDOW, ([this](wxMouseEvent& e) {
+             SetForegroundColour(m_normalColor);
+             Refresh();
+         }));
 }
 
-void HyperLink::SetFont(wxFont& font)
-{   // ensure it stays underlined
+bool HyperLink::SetFont(const wxFont& font)
+{ // ensure it stays underlined
     wxFont f = font;
     f.SetUnderlined(true);
-    wxStaticText::SetFont(f);
+    return wxStaticText::SetFont(f);
 }
 
-void HyperLink::SetURL(const wxString& url) {
+void HyperLink::SetURL(const wxString& url)
+{
     m_url = url;
     SetToolTip(m_url);
 }
 
-wxString HyperLink::GetURL() const{
-    return m_url;
-}
+wxString HyperLink::GetURL() const { return m_url; }
 
-}
-}
+}} // namespace Slic3r::GUI
--- src/slic3r/GUI/Widgets/HyperLink.hpp
+++ src/slic3r/GUI/Widgets/HyperLink.hpp
@@ -3,33 +3,24 @@
 
 #include <wx/wx.h>
 #include <wx/window.h>
-#include "Label.hpp"
 
-namespace Slic3r {
-namespace GUI {
+namespace Slic3r { namespace GUI {
 
 class HyperLink : public wxStaticText
 {
 public:
-    HyperLink(
-        wxWindow* parent,
-        const wxString& label = wxEmptyString,
-        const wxString& url = wxEmptyString,
-        const long style = 0
-);
+    HyperLink(wxWindow* parent, const wxString& label = wxEmptyString, const wxString& url = wxEmptyString, const long style = 0);
 
     void     SetURL(const wxString& url);
     wxString GetURL() const;
 
-    void     SetFont(wxFont& font);
+    bool SetFont(const wxFont& font);
 
 private:
     wxString m_url;
     wxColour m_normalColor;
     wxColour m_hoverColor;
-
 };
 
-}
-}
+}} // namespace Slic3r::GUI
 #endif // !slic3r_GUI_HyperLink_hpp_
