842a48c5f31ca90c83e52d9519c96dde2523fffe
Sabriel-Koh <sabrielkcr@gmail.com>
2026-01-21 17:21:22 +0800

    Fix: rewrite data validation to preserve the function of the original

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -6387,7 +6387,8 @@ void Tab::save_preset(std::string name /*= ""*/, bool detach, bool save_to_proje
     }
 
     //BBS record current preset name
-    std::string curr_preset_name = m_presets->get_edited_preset().name;
+    Preset& edited_preset = m_presets->get_edited_preset();
+    std::string curr_preset_name = edited_preset.name;
 
     bool exist_preset = false;
     Preset* new_preset = m_presets->find_preset(name, false);
@@ -6395,18 +6396,14 @@ void Tab::save_preset(std::string name /*= ""*/, bool detach, bool save_to_proje
         exist_preset = true;
     }
 
-    Preset* _curr_preset = nullptr;
-    if (m_presets->type() == Preset::TYPE_FILAMENT) {
-        // Orca: check if compatible_printers/prints exists and is not empty, set it to the current printer/print if it is empty
-        // When the checkbox is TRUE (i.e., compatible with ALL), the list is already emptied on_toggle
-        if (!m_compatible_printers.checkbox->GetValue()) {
-            _curr_preset = const_cast<Preset*>(&wxGetApp().preset_bundle->printers.get_selected_preset_base());
-            this->load_key_value(m_compatible_printers.key_list, std::vector<std::string> { _curr_preset->name });
-        }
-        if (!m_compatible_prints.checkbox->GetValue()) {
-            _curr_preset = const_cast<Preset*>(&wxGetApp().preset_bundle->prints.get_selected_preset_base());
-            this->load_key_value(m_compatible_prints.key_list, std::vector<std::string> { _curr_preset->name });
-        }
+    // Orca: check if compatible_printers exists and is not empty, set it to the current printer if it is empty
+    // Ensures that custom filaments based on system are not accidentally allowed for all printers
+    // Can still be set for all after creation
+    if (m_presets->type() == Preset::TYPE_FILAMENT && !exist_preset && edited_preset.is_system) {
+        Preset* _curr_printer = const_cast<Preset*>(&wxGetApp().preset_bundle->printers.get_selected_preset_base());
+        ConfigOptionStrings* compatible_printers = m_config->option<ConfigOptionStrings>("compatible_printers");
+        if (nullptr != _curr_printer && compatible_printers && compatible_printers->values.empty())
+            compatible_printers->values.push_back(_curr_printer->name);
     }
     // Save the preset into Slic3r::data_dir / presets / section_name / preset_name.json
     m_presets->save_current_preset(name, detach, save_to_project, nullptr);
