fc2ac47765ae1d97da02583fd671124057b0bd86
Neo Cheng <156867325+neo-jayfeather@users.noreply.github.com>
2026-01-28 18:51:11 -0500

    Add additional resources from BBL (check for CKEditor deps later)
    temporarily add all changed dependencies for resources in BBL from 2.3.1 to 2.4.0 (H2D introduction update)

--- src/slic3r/Utils/MacDarkMode.mm
+++ src/slic3r/Utils/MacDarkMode.mm
@@ -151,10 +151,9 @@ void openFolderForFile(wxString const & file)
 - (BOOL)performDragOperation2:(id<NSDraggingInfo>)info
 {
     NSURL* url = [NSURL URLFromPasteboard:[info draggingPasteboard]];
-    if (!url) {
-        return FALSE;
-    }
     NSString * path = [url path];
+    if (path == nil)
+        return TRUE;
     url = [NSURL fileURLWithPath: path];
     [self loadFileURL:url allowingReadAccessToURL:url];
     return TRUE;
@@ -423,6 +422,6 @@ void StaticGroup_layoutBadge(void * group, void * badge)
     NSView * vg = (NSView *)group;
     NSView * vb = (NSView *)badge;
     vb.translatesAutoresizingMaskIntoConstraints = NO;
-    [vg addConstraint: [NSLayoutConstraint constraintWithItem:vb attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:vg attribute:NSLayoutAttributeTop multiplier:1.0 constant:15]];
+    [vg addConstraint: [NSLayoutConstraint constraintWithItem:vb attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:vg attribute:NSLayoutAttributeTop multiplier:1.0 constant:-1]];
     [vg addConstraint: [NSLayoutConstraint constraintWithItem:vb attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:vg attribute:NSLayoutAttributeRight multiplier:1.0 constant:-1]];
 }
