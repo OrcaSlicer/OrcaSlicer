8e39e5e72c1f7e8c0a8be0825a2a606eb8ebca87
Ian Bassi <ian.bassi@outlook.com>
2026-02-03 17:43:38 -0300

    BY10 to BYCGAL Full refactor

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -112,7 +112,6 @@
 #include "ConfigWizard.hpp"
 #include "SyncAmsInfoDialog.hpp"
 #include "../Utils/ASCIIFolding.hpp"
-#include "../Utils/FixModelByWin10.hpp"
 #include "../Utils/UndoRedo.hpp"
 #include "../Utils/PresetUpdater.hpp"
 #include "../Utils/Process.hpp"
