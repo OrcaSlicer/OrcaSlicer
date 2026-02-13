2dc1b0692faefe9612ba6ea12663993367590396
RF47 <162915171+RF47@users.noreply.github.com>
2026-02-13 19:22:02 -0300

    solve bump artifact in extrusion junction joint

--- src/libslic3r/Feature/FuzzySkin/FuzzySkin.cpp
+++ src/libslic3r/Feature/FuzzySkin/FuzzySkin.cpp
@@ -111,7 +111,7 @@ void fuzzy_polyline(Points& poly, bool closed, coordf_t slice_z, const FuzzySkin
 }
 
 // Thanks Cura developers for this function.
-void fuzzy_extrusion_line(Arachne::ExtrusionJunctions& ext_lines, coordf_t slice_z, const FuzzySkinConfig& cfg)
+void fuzzy_extrusion_line(Arachne::ExtrusionJunctions& ext_lines, coordf_t slice_z, const FuzzySkinConfig& cfg, bool closed)
 {
     std::unique_ptr<noise::module::Module> noise = get_noise_module(cfg);
 
@@ -124,9 +124,11 @@ void fuzzy_extrusion_line(Arachne::ExtrusionJunctions& ext_lines, coordf_t slice
     Arachne::ExtrusionJunctions out;
     out.reserve(ext_lines.size());
     for (auto& p1 : ext_lines) {
+        if (closed) {
         if (p0->p == p1.p) { // Connect endpoints.
             out.emplace_back(p1.p, p1.w, p1.perimeter_index);
             continue;
+        }
         }
 
         // 'a' is the (next) new point between p0 and p1
@@ -418,11 +420,18 @@ void apply_fuzzy_skin(Arachne::ExtrusionLine* extrusion, const PerimeterGenerato
                     extrusion->junctions.clear();
 
                     const auto fuzzy_current_segment = [&segment, &extrusion, &r, slice_z]() {
-                        extrusion->junctions.push_back(segment.front());
+                        const auto front = segment.front();
                         const auto back = segment.back();
-                        fuzzy_extrusion_line(segment, slice_z, r.first);
+                        
+                        fuzzy_extrusion_line(segment, slice_z, r.first, false);
+                        if (!extrusion->junctions.empty() && extrusion->junctions.front().p != front.p) {
+                            extrusion->junctions.push_back(front);
+                        }
                         extrusion->junctions.insert(extrusion->junctions.end(), segment.begin(), segment.end());
-                        extrusion->junctions.push_back(back);
+
+                        if (extrusion->junctions.back().p == back.p) {
+                            extrusion->junctions.push_back(back);
+                        }
                         segment.clear();
                     };
 
@@ -448,6 +457,9 @@ void apply_fuzzy_skin(Arachne::ExtrusionLine* extrusion, const PerimeterGenerato
                     }
                     if (!segment.empty()) {
                         fuzzy_current_segment();
+                    }
+                    if (!extrusion->junctions.empty() && extrusion->junctions.front().p != extrusion->junctions.back().p) {
+                        extrusion->junctions.back().p = extrusion->junctions.front().p;
                     }
                 }
             }
