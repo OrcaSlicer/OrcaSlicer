1a2117506f7b1401dd0fac0ec670e46b0627612c
RF47 <162915171+RF47@users.noreply.github.com>
2026-02-14 00:10:11 -0300

    Clipper2 FillLightning

--- src/libslic3r/Fill/FillLightning.cpp
+++ src/libslic3r/Fill/FillLightning.cpp
@@ -1,4 +1,4 @@
-#include "../ClipperUtils.hpp"
+#include "../Clipper2Utils.hpp"
 #include "../Print.hpp"
 #include "../ShortestPath.hpp"
 #include "FillBase.hpp"
@@ -20,7 +20,7 @@ void Filler::_fill_surface_single(
     // Apply multiline offset if needed
     multiline_fill(fill_lines, params, spacing);
 
-    fill_lines = Slic3r::intersection_pl(std::move(fill_lines), expolygon);
+    fill_lines = Slic3r::intersection_pl_2(std::move(fill_lines), to_polygons(expolygon));
 
     chain_or_connect_infill(std::move(fill_lines), expolygon, polylines_out, this->spacing, params);
 }
