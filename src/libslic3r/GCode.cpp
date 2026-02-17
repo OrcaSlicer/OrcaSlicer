e0358fb5723a27f3a1d6c9ca2c548e7d3cefb451
RF47 <162915171+RF47@users.noreply.github.com>
2026-02-17 19:45:23 -0300

    Normal brim if multimaterial on first layer

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -5002,7 +5002,7 @@ LayerResult GCode::process_layer(
             if (!brim.entities.empty())
                 objects_with_brim++;
         }
-        is_unified_brim = (objects_with_brim == 1);
+        is_unified_brim = (objects_with_brim == 1 && sequence_by_layer);
 
         if (is_unified_brim) {
             // Find the only object that has brim
