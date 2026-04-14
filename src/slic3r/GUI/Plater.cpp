676a6522e8cdf20eb6ce8a3bf75d2792e207c87f
Noisyfox <timemanager.rick@gmail.com>
2026-04-14 19:36:05 +0800

    Fix macos crash on close

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -16650,7 +16650,7 @@ const GLCanvas3D* Plater::canvas3D() const
 
 GLCanvas3D* Plater::get_view3D_canvas3D()
 {
-    return p->view3D->get_canvas3d();
+    return p ? p->view3D->get_canvas3d() : nullptr;
 }
 
 GLCanvas3D* Plater::get_preview_canvas3D()
