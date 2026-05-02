b3a4f7424ad6557d9475c8db651dc370403f4011
Ian Bassi <ian.bassi@outlook.com>
2026-05-02 13:25:04 -0300

    Clarify tooltip text for Z parameters

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -4131,8 +4131,9 @@ void PrintConfigDef::init_fff_params()
     def = this->add("zaa_minimize_perimeter_height", coFloat);
     def->label    = L("Minimize wall height angle");
     def->category = L("Quality");
-    def->tooltip  = L("Reduce height of top surface perimeters to match height of model edge.\n"
-                       "Effects perimeters whose slope is less than this angle in degrees. Reasonable value is 35. Set 0 to disable.");
+    def->tooltip  = L("Reduce the height of top-surface perimeters to match the model edge height.\n"
+                      "Affects perimeters with a slope less than this angle (degrees).\n"
+                      "A reasonable value is 35. Set to 0 to disable.");
     def->sidetext = L("°");
     def->min      = 0;
     def->max      = 90;
@@ -4149,7 +4150,8 @@ void PrintConfigDef::init_fff_params()
     def = this->add("zaa_min_z", coFloat);
     def->label    = L("Minimum z height");
     def->category = L("Quality");
-    def->tooltip  = L("Minimum z layer height.\nAlso controls slicing plane.");
+    def->tooltip  = L("Minimum Z-layer height.\n"
+                      "Also controls the slicing plane.");
     def->sidetext = L("mm");
     def->min      = 0;
     def->max      = 100;
