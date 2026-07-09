bdba074239ae0040619d3357868ac039024a272c
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2026-07-09 22:48:41 +0200

    Update ZLIB version to 1.3.2 in CMake file
    fix (CVE-2023-2804)
    full changelog:
    
    https://github.com/libjpeg-turbo/libjpeg-turbo/releases?page=1

--- deps/ZLIB/ZLIB.cmake
+++ deps/ZLIB/ZLIB.cmake
@@ -2,11 +2,9 @@ set(patch_command git init && ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-Respec
 
 orcaslicer_add_cmake_project(ZLIB
   # GIT_REPOSITORY https://github.com/madler/zlib.git
-  # GIT_TAG v1.2.11
-  #URL https://github.com/madler/zlib/archive/refs/tags/v1.2.11.zip
-  URL https://github.com/madler/zlib/archive/refs/tags/v1.2.13.zip
-  #URL_HASH SHA256=f5cc4ab910db99b2bdbba39ebbdc225ffc2aa04b4057bc2817f1b94b6978cfc3
-  URL_HASH SHA256=c2856951bbf30e30861ace3765595d86ba13f2cf01279d901f6c62258c57f4ff
+  # GIT_TAG v1.3.2
+  URL https://github.com/madler/zlib/archive/refs/tags/v1.3.2.zip
+  URL_HASH SHA256=31fd9fee98812abcf147d0e103bc4d2f983c35a8d7a807a328a299f3a74e0050
   PATCH_COMMAND ${patch_command}
   CMAKE_ARGS
     -DSKIP_INSTALL_FILES=ON         # Prevent installation of man pages et al.
