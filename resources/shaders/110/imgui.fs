bambulab/BambuStudio

FIX: plater toolbar image update
jira: STUDIO-12457

Change-Id: Ia855b4bd9bec884d52202f5d8f5cfad9efce0f2f

resources/shaders/110/imgui.fs
@@ -1,11 +1,11 @@
 #version 110
 
-uniform sampler2D Texture;
+uniform sampler2D s_texture;
 
 varying vec2 Frag_UV;
 varying vec4 color;
 
 void main()
 {
-    gl_FragColor = color * texture2D(Texture, Frag_UV.st);
+    gl_FragColor = color * texture2D(s_texture, Frag_UV.st);
 }

resources/shaders/140/imgui.fs
@@ -1,6 +1,6 @@
 #version 140
 
-uniform sampler2D Texture;
+uniform sampler2D s_texture;
 
 in vec2 Frag_UV;
 in vec4 color;
@@ -8,5 +8,5 @@ in vec4 color;
 out vec4 frag_color;
 void main()
 {
-    frag_color = color * texture(Texture, Frag_UV.st);
+    frag_color = color * texture(s_texture, Frag_UV.st);
 }

OrcaSlicer/OrcaSlicer

(cherry picked from commit 474691813b497654ef701fe7073992db366e81a9)

resources/shaders/110/imgui.fs
@@ -1,11 +1,11 @@
 #version 110
 
-uniform sampler2D Texture;
+uniform sampler2D s_texture;
 
 varying vec2 Frag_UV;
 varying vec4 Frag_Color;
 
 void main()
 {
-	gl_FragColor = Frag_Color * texture2D(Texture, Frag_UV.st);
+    gl_FragColor = Frag_Color * texture2D(s_texture, Frag_UV.st);
 }

resources/shaders/140/imgui.fs
@@ -1,6 +1,6 @@
 #version 140
 
-uniform sampler2D Texture;
+uniform sampler2D s_texture;
 
 in vec2 Frag_UV;
 in vec4 Frag_Color;
@@ -9,5 +9,5 @@ out vec4 out_color;
 
 void main()
 {
-	out_color = Frag_Color * texture(Texture, Frag_UV.st);
+    out_color = Frag_Color * texture(s_texture, Frag_UV.st);
 }