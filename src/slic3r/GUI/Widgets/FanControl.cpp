b3523d7b3677225100f40cfdc11730af41bd4853
Ian Bassi <ian.bassi@outlook.com>
2026-07-22 19:14:43 -0300

    Update localizations
    Add context for a Hotend differenciation and update all translations

--- src/slic3r/GUI/Widgets/FanControl.cpp
+++ src/slic3r/GUI/Widgets/FanControl.cpp
@@ -1037,7 +1037,7 @@ wxString FanControlPopupNew::get_fan_func_name(int mode, int submode, AIR_FUN fu
 
     static std::map<AIR_FUN, wxString> s_fan_func_name
     {
-        { AIR_FUN::FAN_HEAT_BREAK_0_IDX,     _L("Hotend") },
+        { AIR_FUN::FAN_HEAT_BREAK_0_IDX,     _CTX("Hotend", "Hotend Heat Breaker Fan") },
         { AIR_FUN::FAN_COOLING_0_AIRDOOR,     _L("Parts") },
         { AIR_FUN::FAN_REMOTE_COOLING_0_IDX,  _L("Aux") },
         { AIR_FUN::FAN_CHAMBER_0_IDX,         _L("Exhaust") },
