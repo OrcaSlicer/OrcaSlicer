d286807908da700817dd2a0a7312a859a9b8b7ae
Noisyfox <timemanager.rick@gmail.com>
2026-07-24 18:04:03 +0800

    Fix build failure caused by https://github.com/wxWidgets/wxWidgets/commit/436c16135ec7ddf580f44624bf74c592aae43b66

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -1,7 +1,7 @@
 orcaslicer_add_cmake_project(
     wxInspector
     GIT_REPOSITORY https://github.com/Noisyfox/wxInspector.git
-    GIT_TAG        8569ad6401674d1375034321a1e7a93be242ddf7 
+    GIT_TAG        a27c643e8d5d0cf1c8181520c9bb2fd2690a920e 
     GIT_SHALLOW ON
     DEPENDS dep_wxWidgets
     CMAKE_ARGS
