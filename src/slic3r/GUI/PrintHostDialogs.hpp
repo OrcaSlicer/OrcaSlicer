883b7c91ed031184c71fc4a90f906876438f801a
Cart <81428538+cartrigger@users.noreply.github.com>
2026-07-23 22:59:12 -0400

    Add support for FlashForge Flow Calibration

--- src/slic3r/GUI/PrintHostDialogs.hpp
+++ src/slic3r/GUI/PrintHostDialogs.hpp
@@ -271,12 +271,14 @@ private:
     bool                             m_leveling_before_print {true};
     bool                             m_time_lapse_video {false};
     bool                             m_use_material_station {false};
+    bool                             m_flow_calibration {false};
     bool                             m_supports_material_station {false};
     bool                             m_slots_loaded {false};
 
-    const char* CONFIG_KEY_LEVELING  = "flashforge_leveling_before_print";
-    const char* CONFIG_KEY_TIMELAPSE = "flashforge_timelapse_video";
-    const char* CONFIG_KEY_IFS       = "flashforge_use_material_station";
+    const char* CONFIG_KEY_LEVELING   = "flashforge_leveling_before_print";
+    const char* CONFIG_KEY_TIMELAPSE  = "flashforge_timelapse_video";
+    const char* CONFIG_KEY_IFS        = "flashforge_use_material_station";
+    const char* CONFIG_KEY_FLOWCAL    = "flashforge_flow_calibration";
 };
 
 wxDECLARE_EVENT(EVT_PRINTHOST_PROGRESS, PrintHostQueueDialog::Event);
