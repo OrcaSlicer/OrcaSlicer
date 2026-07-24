0d69550c32ad55df77e56fbfc4ec408f58ff20a9
Noisyfox <timemanager.rick@gmail.com>
2026-07-24 17:11:20 +0800

    Attempt to fix build again

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -1,7 +1,7 @@
 orcaslicer_add_cmake_project(
     wxInspector
     GIT_REPOSITORY https://github.com/Noisyfox/wxInspector.git
-    GIT_TAG        e27ed8eca4e68cd1e64e20328a9706d97668eb65 
+    GIT_TAG        8569ad6401674d1375034321a1e7a93be242ddf7 
     GIT_SHALLOW ON
     DEPENDS dep_wxWidgets
     CMAKE_ARGS
