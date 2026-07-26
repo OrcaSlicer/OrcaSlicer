85af3f583a132e9814b968a3374b577d1de1863f
Noisyfox <timemanager.rick@gmail.com>
2026-07-26 16:56:27 +0800

    Use wxInspector 1.0.0 release

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -1,9 +1,8 @@
 orcaslicer_add_cmake_project(
     wxInspector
-    GIT_REPOSITORY https://github.com/Noisyfox/wxInspector.git
-    GIT_TAG        4e4ad7dcbaa77e1680e8a22eb66cbeac58013a4a 
-    GIT_SHALLOW ON
-    DEPENDS dep_wxWidgets
+    URL https://github.com/Noisyfox/wxInspector/archive/refs/tags/v1.0.0.zip
+    URL_HASH SHA256=0ba163956f2d468b19a91b96c5aba66ee9610843ea41dda628ea44cdafde7db7
+    DEPENDS ${WXWIDGETS_PKG}
     CMAKE_ARGS
         -DCMAKE_CXX_FLAGS="-DwxDEBUG_LEVEL=0"
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
