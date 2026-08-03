171ca60f083bc91ca8695fcf3e0e45c4a2a012b6
anjis <anjis.zhou@elegoo.com>
2026-08-03 18:08:13 +0800

    Add cross-platform option to disable 3D mouse support

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -248,8 +248,8 @@ void AppConfig::set_defaults()
     if (get("reverse_mouse_wheel_zoom").empty())
         set_bool("reverse_mouse_wheel_zoom", false);
 
-    if (get("skip_3dmouse_detect").empty())
-        set_bool("skip_3dmouse_detect", false);
+    if (get("disable_3dmouse").empty())
+        set_bool("disable_3dmouse", false);
 
     if (get("enable_append_color_by_sync_ams").empty())
         set_bool("enable_append_color_by_sync_ams", true);
--- src/slic3r/GUI/Preferences.cpp
+++ src/slic3r/GUI/Preferences.cpp
@@ -1793,13 +1793,11 @@ void PreferencesDialog::create_items()
     auto reverse_mouse_zoom    = create_item_checkbox(_L("Reverse mouse zoom"), _L("If enabled, reverses the direction of zoom with mouse wheel."), "reverse_mouse_wheel_zoom");
     g_sizer->Add(reverse_mouse_zoom);
 
-#ifdef _WIN32
-    auto item_skip_3dmouse_detect = create_item_checkbox(
-        _L("Skip 3D mouse detection"),
-        _L("Prevents detection of and connection to 3D mouse devices. Enable this option if 3D mouse detection causes startup or compatibility issues."),
-        "skip_3dmouse_detect", _L("(Requires restart)"));
-    g_sizer->Add(item_skip_3dmouse_detect);
-#endif
+    auto item_disable_3dmouse = create_item_checkbox(
+        _L("Disable 3D mouse support"),
+        _L("Disables 3D mouse device detection and input."),
+        "disable_3dmouse", _L("(Requires restart)"));
+    g_sizer->Add(item_disable_3dmouse);
 
     std::vector<wxString> ButtonDragActions = {_L("None"), _L("Pan"), _L("Rotate")};
     auto item_left_mouse_drag  = create_item_combobox(_L("Left Mouse Drag"), _L("Set the action that dragging the left mouse button should perform."), "left_mouse_drag_action", ButtonDragActions);
