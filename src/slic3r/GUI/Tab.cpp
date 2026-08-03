FreddieSparrow/Adaptive-Detail-Printing-ADP-Slicer

Add feature to skip CONFIG_BLOCK in G-code files

src/slic3r/GUI/Tab.cpp
@@ -2689,6 +2689,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("reduce_infill_retraction", "others_settings_g_code_output#reduce-infill-retraction");
         optgroup->append_single_option_line("gcode_add_line_number", "others_settings_g_code_output#add-line-number");
         optgroup->append_single_option_line("gcode_comments", "others_settings_g_code_output#verbose-g-code");
+        optgroup->append_single_option_line("gcode_skip_config_block", "others_settings_g_code_output#skip-g-code-config-block");
         optgroup->append_single_option_line("gcode_label_objects", "others_settings_g_code_output#label-objects");
         optgroup->append_single_option_line("exclude_object", "others_settings_g_code_output#exclude-objects");
         option = optgroup->get_option("filename_format");

OrcaSlicer/OrcaSlicer

src/slic3r/GUI/Tab.cpp
@@ -4849,6 +4849,7 @@ void TabPrinter::build_fff()
 
         optgroup->append_single_option_line("printer_structure", "printer_basic_information_advanced#printer-structure");
         optgroup->append_single_option_line("gcode_flavor", "printer_basic_information_advanced#g-code-flavor");
+        optgroup->append_single_option_line("gcode_skip_config_block", "printer_basic_information_advanced#skip-g-code-config-block");
         optgroup->append_single_option_line("pellet_modded_printer", "printer_basic_information_advanced#pellet-modded-printer");
         optgroup->append_single_option_line("bbl_use_printhost", "printer_basic_information_advanced#use-3rd-party-print-host");
         optgroup->append_single_option_line("use_3mf");