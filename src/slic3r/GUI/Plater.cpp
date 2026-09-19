52fa7bb278f6a13c234b6d55f4c971990a56eaa9
raistlin7447 <kris.austin@gmail.com>
2026-09-18 21:43:21 -0500

    gui: pop_warning_and_go_to_device_page never took the device-page branch
    The condition tested mainframe == nullptr before dereferencing it, so
    device_page was always false and the "go to the device page" wording
    never showed.

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -20843,8 +20843,8 @@ void Plater::pop_warning_and_go_to_device_page(wxString printer_name, PrinterWar
 {
     printer_name.Replace("Bambu Lab", "", false);
     wxString content;
-    bool device_page = (wxGetApp().mainframe == nullptr) && (MonitorPanel::if_built() != nullptr) &&
-                       (MonitorPanel::if_built()->IsShown());
+    MonitorPanel* monitor     = MonitorPanel::if_built();
+    bool          device_page = monitor != nullptr && monitor->IsShown();
     if (type == PrinterWarningType::NOT_CONNECTED) {
         if (device_page) {
             content = wxString::Format(_L("Printer not connected. Please go to the device page to connect %s before syncing."),
