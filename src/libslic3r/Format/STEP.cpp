d1d38e34511ebaa8d79acae980beba1bb1f344d7
Kenneth Raplee <kenrap@FreeBSD.org>
2026-09-22 23:41:18 -0700

    Add missing TDF_LabelSequence include in STEP.cpp

--- src/libslic3r/Format/STEP.cpp
+++ src/libslic3r/Format/STEP.cpp
@@ -25,6 +25,7 @@
 #include "XCAFDoc_DocumentTool.hxx"
 #include "XCAFDoc_ShapeTool.hxx"
 #include "XCAFApp_Application.hxx"
+#include "TDF_LabelSequence.hxx"
 #include "TopoDS_Solid.hxx"
 #include "TopoDS_Compound.hxx"
 #include "TopoDS_Builder.hxx"
